75afd876b10b1ace5bd845690fec01eecdfe1903
abhinav-TB <abhinavtb@gmail.com>
2020-03-16 12:02:09 +0530

    solvend servent problem

---
+++ code chef beginner/servant.cpp
@@ -0,0 +1,18 @@
+# include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a;
+    cin>>n;
+    while(n!=0)
+    {
+        cin>>a;
+        if(a<10)
+        cout<<"What an obedient servant you are"<<endl;
+        else
+        cout<<"-1"<<endl;;
+        n--;
+        
+    }
+
+}
