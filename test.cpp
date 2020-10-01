6c6c64b12f027dfc9bbf1563d21f3e4b4f4354e4
abhinav-TB <abhinavtb@gmail.com>
2020-10-01 19:36:29 +0530

    two numbers problem solved

--- test.cpp
+++ test.cpp
@@ -2,81 +2,11 @@
 
 using namespace std;
 
-// Complete the minimumNumber function below.
-int minimumNumber(int n, string password) {
-    // Return the minimum number of characters to make the password strong
-   string numbers = "0123456789",lower_case = "abcdefghijklmnopqrstuvwxyz",
-    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    special_characters = "!@#$%^&*()-+";
-     
-    bool num ,upper,lower,special =false;
-    int count =4;
-    for (int i=0;i<n;i++)
-    {    
-       if(!num)
-       {
-         size_t pos = numbers.find(password[i]);
-         if(pos !=string::npos)
-           {
-               num =true;
-               count--;
-           }
-       }
-       if(!upper)
-       {
-         
-         if(isupper(password[i]))
-           {
-               upper =true;
-               count--;
-           }
-       }
-       if(!lower)
-       {
-         
-         if(islower(password[i]))
-           {
-               lower =true;
-               count--;
-           }
-       }
-       if(!special)
-       {
-         size_t pos =  special_characters.find(password[i]);
-         if(pos !=string::npos)
-           {
-               special =true;
-               count--;
-           }
-       }
-    }
-
-    if((n+count)<6){
-
-       return (6-n);
-    }
-    else
-    return count;
-    
-    
-}
-
 int main()
 {
-
-
-    int n;
-    cin >> n;
-
-
-    string password;
-    cin>>password;
-
-    int answer = minimumNumber(n, password);
-
-    cout << answer << "\n";
-
-   
-
-    return 0;
+  string s ="asaaaaaag";
+  sort(s.begin(), s.end()); 
+  auto it =unique(s.begin(),s.end());
+  s.resize(distance(s.begin(),it));
+  cout<<s;
 }
