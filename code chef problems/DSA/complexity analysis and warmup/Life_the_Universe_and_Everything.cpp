1012c80b26ed7104499b6523237fd7da45efac44
abhinav-TB <abhinavtb@gmail.com>
2020-10-21 19:24:00 +0530

    new file added

---
+++ code chef problems/DSA/complexity analysis and warmup/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define ll long long;
+
+int main(){
+    while(1){
+        int n;
+        cin>>n;
+        if(n==42){
+            break;
+        }
+        cout<<n<<endl;
+    }
+}
