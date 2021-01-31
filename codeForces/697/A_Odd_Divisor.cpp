4a9679a76c195d91d82551b560599333591ae5d0
abhinav-TB <abhinavtb@gmail.com>
2021-01-31 21:49:40 +0530

    more problems solved😀

---
+++ codeForces/697/A_Odd_Divisor.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+
+
+void solve() {
+ ll n;
+ cin>>n;
+ while(n % 2 == 0){
+     n/=2;
+ }
+
+ if(n != 1) {
+
+     cout << "YES" <<endl;
+ }
+ else {
+
+     cout << "NO" <<endl;
+ }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int tc; cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t  << ": ";
+        solve();
+    }
+}
